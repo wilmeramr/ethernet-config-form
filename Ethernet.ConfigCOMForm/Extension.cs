23df55aa9ef3e2b4b9ae205b9ab1ae8f14f9fbfb
wrojas <huen6y43njvkgqjbkkvimz2m56lpf6d5q6frdbpun3bcu26gvmlq>
2021-08-30 20:56:03 -0300

    add dns, mask,port
    add dns, mask,port

--- Ethernet.ConfigCOMForm/Extension.cs
+++ Ethernet.ConfigCOMForm/Extension.cs
@@ -27,5 +27,45 @@ namespace Ethernet.ConfigCOMForm
             return Convert.ToInt64(hexValue, 16).ToString(); 
         }
 
+
+        public static long decimalBinario(this int numero)
+        {
+
+            long binario = 0;
+
+            const int DIVISOR = 2;
+            long digito = 0;
+
+            for (int i = numero % DIVISOR, j = 0; numero > 0; numero /= DIVISOR, i = numero % DIVISOR, j++)
+            {
+                digito = i % DIVISOR;
+                binario += digito * (long)Math.Pow(10, j);
+            }
+
+
+            return binario;
+        }
+
+
+        public static long binarioDecimal(this long binario)
+        {
+
+            long numero = 0;
+            long digito = 0;
+            const int DIVISOR = 10;
+
+            for (long i = binario, j = 0; i > 0; i /= DIVISOR, j++)
+            {
+                digito = (long)i % DIVISOR;
+                if (digito != 1 && digito != 0)
+                {
+                    return -1;
+                }
+                numero += digito * (int)Math.Pow(2, j);
+            }
+
+            return numero;
+        }
+
     }
 }
